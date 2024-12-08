ec9ca2f1cfa990dc1f828d75c6beb95c4d8a381e
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-08 13:11:23 +0545

    spinner

--- app/src/main/java/com/example/a35d/SignupActivity.kt
+++ app/src/main/java/com/example/a35d/SignupActivity.kt
@@ -2,6 +2,8 @@
 package com.example.a35d
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
@@ -10,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignupActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
     lateinit var spinner: Spinner
     lateinit var textView: TextView
 
@@ -18,6 +20,7 @@ class SignupActivity : AppCompatActivity() {
         "Nepal","China","Bhutan","Pakistan",
         "Canada","Australia","India"
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -34,11 +37,24 @@ class SignupActivity : AppCompatActivity() {
             android.R.layout.simple_dropdown_item_1line)
 
         spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        if(parent != null){
+            textView.text =
+                parent.getItemAtPosition(position).toString()
+
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
     }
 }
