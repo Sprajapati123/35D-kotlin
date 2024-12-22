15a43d3852bc8b73bd0ca6830aa507d96c53d257
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-22 14:09:38 +0545

    fragment

--- app/src/main/java/com/example/a35d/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/a35d/ui/activity/HomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.a35d.R
@@ -19,22 +20,14 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(FirstFragment())
 
         binding.btnFirst.setOnClickListener {
-            val fragmentManager : FragmentManager = supportFragmentManager
-            val fragmentTransaction: FragmentTransaction = fragmentManager.
-                                                            beginTransaction()
-            fragmentTransaction.replace(R.id.frameHome,FirstFragment())
-            fragmentTransaction.commit()
+            replaceFragment(FirstFragment())
 
         }
         binding.btnSecond.setOnClickListener {
-            val fragmentManager : FragmentManager = supportFragmentManager
-            val fragmentTransaction: FragmentTransaction = fragmentManager.
-                                                        beginTransaction()
-            fragmentTransaction.replace(R.id.frameHome,SecondFragment())
-            fragmentTransaction.commit()
-
+            replaceFragment(SecondFragment())
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -42,5 +35,13 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.
+        beginTransaction()
+        fragmentTransaction.replace(R.id.frameHome,fragment)
+        fragmentTransaction.commit()
     }
 }
